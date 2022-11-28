6d836bbcd1964c6317bf437ca054140ba263ef8e
vinapatil1912 <patil.vina191288@gmail.com>
2022-11-28 17:31:19 +0530

    Initial commit

---
+++ Test1.java
@@ -0,0 +1,15 @@
+class EmployeeTest{
+
+    private int empId;
+    private String empName;
+
+    EmployeeTest(int id,String name){
+        empId=id;
+        empName=name;
+    }
+
+    public void display(){
+        System.out.println(empId);
+        System.out.println(empName);
+    }
+}
